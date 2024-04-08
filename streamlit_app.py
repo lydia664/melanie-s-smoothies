59f22588ec592468cfd3f575c84d98c091f4c3d8
uvsq21920110 <75782969+lydia664@users.noreply.github.com>
2024-04-08 16:23:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
 
 
 
-        
+       st.stop() 
         time_to_insert= st.button('Submit Order!')
         if time_to_insert:
             session.sql(my_insert_stmt).collect()
