13b3afecbbc788147599c928ec70d08e9d52b77b
uvsq21920110 <75782969+lydia664@users.noreply.github.com>
2024-04-08 15:36:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,9 @@ session=cnx.session()
 my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe,use_container_width=True)
 #st.stop()
-
 pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
+st.dataframe(pd_df)
+st.stop()
 ingredients_list=st.multiselect('Choose up to 5 ingredients:', my_dataframe,max_selections=5 )
 if ingredients_list:
     ingredients_string=''
@@ -43,7 +42,7 @@ if ingredients_list:
                                 values('"""+ ingredients_string + """','""" + name_on_order+ """')"""
         #st.write(my_insert_stmt)   
         
-        time_to_insert= st.button('Submit Order!')
+time_to_insert= st.button('Submit Order!')
         if time_to_insert:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered,'+ name_on_order+ " !",icon="✅")
